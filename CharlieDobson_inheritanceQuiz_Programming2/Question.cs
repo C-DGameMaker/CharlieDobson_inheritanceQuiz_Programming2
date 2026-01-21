6b0bdb45dedcfd4b401cb3a5ef9bcbd0c1bc28af
C-DGameMaker <charlotte5634@outlook.com>
2026-01-21 08:05:20 -0400

    Initial commit

---
+++ CharlieDobson_inheritanceQuiz_Programming2/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieDobson_inheritanceQuiz_Programming2
+{
+    internal class Question
+    {
+        protected string _questionText;
+        protected string _answerText;
+
+        public Question()
+        {
+            _questionText;
+            _answerText;
+        }
+
+        public void Ask()
+        {
+            Console.WriteLine(_questionText);
+        }
+
+
+    }
+}
