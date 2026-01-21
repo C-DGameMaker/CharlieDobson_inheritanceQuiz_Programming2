11be2f18e194dd98f67eee8d5bdd7ec306e7e6b9
C-DGameMaker <charlotte5634@outlook.com>
2026-01-21 09:03:01 -0400

    Finished Multiple Choice Constructor; Started gameplay loop

--- CharlieDobson_inheritanceQuiz_Programming2/MultipleChoiceQuestion.cs
+++ CharlieDobson_inheritanceQuiz_Programming2/MultipleChoiceQuestion.cs
@@ -11,16 +11,20 @@ namespace CharlieDobson_inheritanceQuiz_Programming2
         private string[] _answerOptions;
         private int _correctAnswer;
 
-        public MultipleChoiceQuestion(string questionText, string answerText, int rightAnswer): base(questionText, answerText)
+        public MultipleChoiceQuestion(string questionText, string[] answerText, int rightAnswer): base(questionText, answerText.ToString())
         {
             _questionText = questionText;
             _correctAnswer = rightAnswer;
 
-            //_answerOptions = ;
+            _answerOptions = answerText;
         }
 
         public override bool CheckAnswer()
         {
+            for(int i = 0; i < _answerOptions.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {_answerOptions[1]}");
+            }
             Console.WriteLine("Press the corrosponding number key to select your answer: ");
 
             return base.CheckAnswer();
